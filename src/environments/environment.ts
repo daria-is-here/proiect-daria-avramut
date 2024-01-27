fed319ed69bef0030f4a6cd752fa087bbbbf4a33
daria-is-here <119736114+daria-is-here@users.noreply.github.com>
2024-01-27 21:26:55 +0200

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCOHbutfiJZ7UxoGPtMWmjhQcL2uDpbtbw",
+    authDomain: "proiect-avram.firebaseapp.com",
+    projectId: "proiect-avram",
+    storageBucket: "proiect-avram.appspot.com",
+    messagingSenderId: "627945429673",
+    appId: "1:627945429673:web:91f43232527d24df8ca7b8",
+    measurementId: "G-G3Y1476FTJ"
+  }
+};
